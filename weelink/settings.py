9430e469240aa17c0b741b7ea07076249a7b9f8b
HusanboyUs <husanghost@gmail.com>
2022-03-26 15:31:09 +0100

    providers/final/static

--- weelink/settings.py
+++ weelink/settings.py
@@ -155,6 +155,7 @@ STATICFILES_DIRS=[os.path.join(BASE_DIR, 'static')]
 #for my media files
 MEDIA_ROOT =  os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/' 
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
