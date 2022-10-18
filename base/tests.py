8294d1cb45882b90ceb5181cb12e1087249ef771
HusanboyUs <husanghost@gmail.com>
2022-10-18 02:36:41 +0200

    mixed tests

--- base/tests.py
+++ base/tests.py
@@ -1,11 +1,15 @@
+from lzma import _PathOrFile
 from django.test import TestCase,Client
 from django.urls import reverse
-import json
 from .models import Profile
 
 class TestViews(TestCase):
+    
+
     def test_project_list_GET(self):
         client=Client()
         response=client.get(reverse('lists'))
+        self=total_profiles=Profile.objects.all()
+        
         self.assertEqual(response.status_code, 200)
         
