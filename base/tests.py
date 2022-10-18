6f182f297d4fff30bb78ef867710ead87664de4d
HusanboyUs <husanghost@gmail.com>
2022-10-18 02:45:16 +0200

    fixed test cases

--- base/tests.py
+++ base/tests.py
@@ -1,15 +1,15 @@
-from lzma import _PathOrFile
 from django.test import TestCase,Client
 from django.urls import reverse
 from .models import Profile
 
 class TestViews(TestCase):
-    
+    def setUp(self):
+        self.client=Client()
+        self.detail_url=reverse('profileView')
+        self.profiles=Profile.objects.all()
 
-    def test_project_list_GET(self):
-        client=Client()
-        response=client.get(reverse('lists'))
-        self=total_profiles=Profile.objects.all()
+
+    def test_profiles(self):
         
-        self.assertEqual(response.status_code, 200)
-        
+        self.assertNotEqual(self.profiles.count(), 0)    
+
