97bb034189f2fd747dc201c23f1636a9759d8735
Husanboy Usmonov <70463121+HusanboyUs@users.noreply.github.com>
2022-12-11 18:39:10 +0100

    Update urls.py

--- weelink/urls.py
+++ weelink/urls.py
@@ -4,13 +4,18 @@ from django.urls import path, include,re_path
 from django.conf import settings
 from django.conf.urls.static import static
 from django.views.static import serve
+from django.http import HttpResponse
+
+def userError(request):
+    return HttpResponse('This app is now under development')
 
 urlpatterns = [
     path('adminlar/', admin.site.urls),
-    path('', include('base.urls')),
+    path('soon/', include('base.urls')),
     path('accounts/', include('allauth.urls')),
     #api only
     path('api/', include('api.urls')),
+    path('',userError,name='for now only'),
 ]
 urlpatterns += [
         re_path(r'^media/(?P<path>.*)$', serve, {
